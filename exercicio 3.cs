2a63b8db62be6150ed8f2354e1d94548e2c08c17
rafaelmarques0 <rafael20marques08@gmail.com>
2024-11-07 12:05:56 +0000

    Add files via upload

---
+++ exercicio 3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class exercicio3
+{
+    public static void Main(string[] args)
+    {
+        int numeros1 = 3;
+        int numeros2 = 1;
+        int numeros3 = 6;
+        int numeros4 = 3;
+        Console.WriteLine("olá,voce tem estes numeros");
+        Console.WriteLine("3,1,6,3");
+        if (numeros1 == numeros4) {
+            Console.WriteLine("o numero 1 e igual ao numero 4");
+        }else { }
+    
+    }
+}
