29ae735685df6833b304e8eaad74039e30d81265
rafaelmarques0 <rafael20marques08@gmail.com>
2024-11-07 12:08:58 +0000

    Add files via upload

---
+++ exercicio2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class PrimeiroPrograma
+{
+    public static void Main(string[] args)
+    {
+      int raio = 3;
+        int diametro = 2;
+        Console.WriteLine("o raio do circulo de raio 3 cm e ");
+        Console.WriteLine(raio*diametro);
+    }
+
+
+}
