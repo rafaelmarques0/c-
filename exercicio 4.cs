108331e5f592a98487bd9ba2de2d9f6996269211
rafaelmarques0 <rafael20marques08@gmail.com>
2024-11-08 12:50:34 +0000

    Add files via upload

---
+++ exercicio 4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class exercicio4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        { //exercicio 1\\
+            for (int contador = 0;contador <21; contador++)
+            {
+                if (contador%2 == 0) {
+                 Console.WriteLine(contador);
+                }
+            }
+        }
+    }
+}
