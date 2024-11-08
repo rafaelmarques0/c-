e2b7ba9d068c5c365d55fa491aa3b225ca4b0f1f
rafaelmarques0 <rafael20marques08@gmail.com>
2024-11-08 12:51:15 +0000

    Add files via upload

---
+++ exercicio4 ex 2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class exercicio4Ex2
+{
+ public static void Main(string[] args)
+ {//exercicio 2\\
+   int soma = 0;
+   for (int contador = 1; contador <= 50; contador++) {
+   if (contador%2 != 0) {
+   soma = soma + contador;}
+   }
+   Console.WriteLine("total de numeros impares="+soma);
+ }
+}
