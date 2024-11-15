6f430796796bbfc9721da48d19bca37e28f00a7d
rafaelmarques0 <rafael20marques08@gmail.com>
2024-11-15 13:05:19 +0000

    Add files via upload

--- exercicio5,3.cs
+++ exercicio5,3.cs
@@ -3,18 +3,18 @@ public class exercicio53
 {
     static void Main(string[] args)
     {
-        int idade = 15;
+        int idade = 65;
         bool isCasado = true;
 
         if (idade >= 65)
         {
             Console.WriteLine("Es senior");
         }
-        else if ( idade >= 18   && isCasado)
+        else if ( idade > 18 && idade <65   && isCasado)
         {
             Console.WriteLine("Es alduto e casado ");
         }
-        else if (idade > 18  && !isCasado ){
+        else if (idade > 18 && idade <65  && !isCasado ){
             Console.WriteLine("es alduto e solteiro");
         }
         else
