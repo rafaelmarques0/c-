b7aa0f1fb81d6b3efb37e34be90e0011ef1b90bc
rafaelmarques0 <rafael20marques08@gmail.com>
2024-11-29 12:58:32 +0000

    Add files via upload

--- exercicio 7.cs
+++ exercicio 7.cs
@@ -1,30 +1,32 @@
 ﻿using System;
 
 public class exercicio7 { 
-        static void Main(string[] args)
-        {int nota = 29;
-        switch(nota)
+        public static void Main(string[] args)
         {
-            case int n when (n >= 17):
-                Console.WriteLine("excelente");
-                break;
+            int nota = 12;
 
-            case int n when (n >= 14):
-                Console.WriteLine("bom");
-                break;
+            switch(nota)
+            {
+                case >= 18 and <= 20:
+                    Console.WriteLine("excelente");
+                    break;
 
-            case int n when (n >= 10):
-                Console.WriteLine("suficiente");
-                break;
+                case >= 14 and <= 17:
+                    Console.WriteLine("bom");
+                    break;
 
-            case int n when (n >= 0):
-                Console.WriteLine("insuficiente");
-                break;
+                case >= 10 and <= 13:
+                    Console.WriteLine("suficiente");
+                    break;
+
+                case >= 0 and <= 9:
+                    Console.WriteLine("insuficiente");
+                    break;
 
                 default:
-                Console.WriteLine("nota inválida");
-                break ;
-        }
+                    Console.WriteLine("nota inválida");
+                    break ;
+            }
         }
     }
 
