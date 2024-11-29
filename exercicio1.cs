d08d840eec8a6273307c3b2fda2d8d06f3276f33
rafaelmarques0 <rafael20marques08@gmail.com>
2024-11-29 11:53:29 +0000

    Add files via upload

---
+++ exercicio1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //variavel
+        int numero1 = 20;
+        int numero2 = 20;
+        //logica
+        Console.WriteLine("os resultados são");
+        Console.WriteLine(numero1 + numero2);
+        Console.WriteLine(numero2 - numero1);
+        Console.WriteLine(numero2 * numero1);
+        Console.WriteLine(numero2 / numero1);
+
+    }
+}
