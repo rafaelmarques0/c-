9835a51ef9e9b188c6e7ee9ac88b5d543034607a
rafaelmarques0 <rafael20marques08@gmail.com>
2024-11-29 12:20:19 +0000

    Add files via upload

---
+++ exercicio 7.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class exercicio7 { 
+        static void Main(string[] args)
+        {int nota = 29;
+        switch(nota)
+        {
+            case int n when (n >= 17):
+                Console.WriteLine("excelente");
+                break;
+
+            case int n when (n >= 14):
+                Console.WriteLine("bom");
+                break;
+
+            case int n when (n >= 10):
+                Console.WriteLine("suficiente");
+                break;
+
+            case int n when (n >= 0):
+                Console.WriteLine("insuficiente");
+                break;
+
+                default:
+                Console.WriteLine("nota inválida");
+                break ;
+        }
+        }
+    }
+
---
+++ texte switch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class teste
+{
+        public static void Main(string[] args)
+        {
+        int dia = 3;
+        switch (dia)
+        {
+            case 1:
+                Console.WriteLine("Hoje voce nao tem reuniao marcada");
+                break;
+            case 2:
+                Console.WriteLine("Hoje voce tem reuniao marcada as 2:20 da tarde");
+                break;
+            case 3:
+                Console.WriteLine("Hoje voce tem reuniao marcada ao meio dia");
+                break;
+            case 4:
+                Console.WriteLine("Hoje voce nao tem reuniao marcada");
+                Console.WriteLine("aviso de ultima hora voce tera reuniao a 10 da noite");
+                break;
+            case 5:
+                Console.WriteLine("Hoje voce nao tem reuniao marcada");
+                break;
+            default:
+                Console.WriteLine("voce tem reuniao online as 7 da manha");
+                break;
+        }
+    }
+}
