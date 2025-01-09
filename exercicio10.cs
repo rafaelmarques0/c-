d9f24703512a9055dec409448422824ccfcf0303
rafaelmarques0 <rafael20marques08@gmail.com>
2025-01-09 16:18:37 +0000

    Add files via upload

---
+++ exercicio10.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class exercicio10
+{
+       public static void Main(string[] args)
+        {
+        int numero = 10;
+        int numero1 = 3;
+       for (int i = 0; i < numero; i++)
+        {
+            Console.WriteLine(i*3);
+        }
+    }
+
+}
