f438bd3b9dc1594660dd114f8cebf6d56acc1fd2
rafaelmarques0 <rafael20marques08@gmail.com>
2025-01-09 17:12:16 +0000

    Add files via upload

---
+++ exercicio11.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+public class exercicio11 { 
+       public static void Main(string[] args)
+        {
+        int soma = 0;
+        int contador = 0;
+        do
+        {
+            soma += contador;
+            Console.WriteLine(soma);
+            contador++;
+        }
+        while (soma <= 100);
+    }
+}
