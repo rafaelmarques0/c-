6bc839f9612788ae2e54e9dd1209adc92757318e
rafaelmarques0 <rafael20marques08@gmail.com>
2025-01-16 16:11:31 +0000

    Add files via upload

---
+++ exemplo break.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class exemplobreak
+{ 
+      public static void Main(string[] args)
+      {
+        for (int contador = 1; contador <= 10; contador++)
+        {
+            if (contador == 5)
+            {
+                break;
+            }
+            Console.WriteLine(contador);
+        }
+      }
+} 
---
+++ exemplo continue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class exemploContinue
+{
+        public  static void Main(string[] args)
+        {
+            for (int contador = 1; contador <= 10; contador++)
+            {
+             
+                if (contador == 5)
+                {
+                    continue;
+                }
+            Console.WriteLine(contador);
+        }
+        }  
+}
