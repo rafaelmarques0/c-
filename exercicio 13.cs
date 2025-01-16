ab724567263522a11813e8014761da4c6a07cbdc
rafaelmarques0 <rafael20marques08@gmail.com>
2025-01-16 15:17:39 +0000

    Add files via upload

---
+++ exercicio 13.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class exercicio13
+{
+    
+    public static void Main(string[] args)
+    {
+        int numero = 1;
+
+        do {
+            Console.WriteLine(numero);
+            numero++;
+           
+        }while (numero <= 5);
+        
+        Console.WriteLine("contagem concluída");
+    }
+}
---
+++ exercicio12.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+public class exercicio12
+{
+     public static void Main(string[] args)
+     {
+       int counter = 1;
+
+       do
+        {
+            //contandor ate 10//
+            Console.WriteLine(counter);
+            counter++;
+        }while (counter <= 10);
+     }
+}
