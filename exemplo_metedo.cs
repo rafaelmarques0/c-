62b0bef31b6f02b5be82e158ca7699eb811bdbe5
rafaelmarques0 <rafael20marques08@gmail.com>
2025-01-23 15:39:35 +0000

    Add files via upload

---
+++ exemplo_metedo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class exemplo_metedo
+{ 
+     public static void Main(string[] args)
+     {
+        int resultado = Soma(10,20);
+
+        Console.WriteLine("a soma é: "+resultado);
+     }
+    // metedo de soma a ser usando//
+    static int Soma(int a,int b)
+    {
+        return a + b;
+    }
+}
---
+++ exercicio 14.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+public class exercicio14
+{  
+        static void Main(string[] args)
+        {
+        int numero = Soma(30, 20);
+        Console.WriteLine("o numero maior é: "+numero);
+        }
+    static int Soma (int a,int b)
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+}
