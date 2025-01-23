3189ad05321108f93a8907ef3046c692ee43500b
rafaelmarques0 <rafael20marques08@gmail.com>
2025-01-23 17:15:24 +0000

    Add files via upload

---
+++ exercicio 15.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class exercicio15
+{
+        static void Main(string[] args)
+        {
+        bool par = Par(10);
+        if (par)
+        {
+            Console.WriteLine("o numero e par");
+        }
+        else 
+        {
+            Console.WriteLine("o numero e ímpar");
+        }
+        }
+    public static bool Par (int a)
+    {
+      return (a%2 == 0);
+    }
+}
