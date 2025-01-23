a41888d7f8f7d0357a4a0e0afd5bb3c2b841a7ba
rafaelmarques0 <rafael20marques08@gmail.com>
2025-01-23 15:43:58 +0000

    Add files via upload

---
+++ exercicio 14.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+public class exercicio14
+{  
+        static void Main(string[] args)
+        {
+        int numero = Maior(30, 20);
+        Console.WriteLine("o numero maior é: "+numero);
+        }
+    static int Maior (int a,int b)
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+}
