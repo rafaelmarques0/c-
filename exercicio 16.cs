d5d5685eb054a537a30db5c42bc0003069b6434f
rafaelmarques0 <rafael20marques08@gmail.com>
2025-01-30 16:14:26 +0000

    Add files via upload

---
+++ exercicio 16.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class exercicio_16
+{
+
+    public static void parImpar(int a)
+    {
+        bool resultado = a % 2 == 0;
+        if (resultado)
+        {
+            quadrado(a);
+        }
+        else
+        {
+            Console.WriteLine("o numero nao e par por isso nao e possivel calcular o quadrado");
+        }
+    }
+    public static void quadrado(int valor)
+    {
+        int resultado = valor * valor;
+       Console.WriteLine("o numero e par e o quadrado é "+ resultado);
+    }
+    public static void Main(string[] args)
+    {
+        parImpar(15);
+        parImpar(10);
+    }
+}
