eb980906488d09c080970b5586b4e4fbe9b9b2f9
rafaelmarques0 <rafael20marques08@gmail.com>
2025-02-05 17:19:03 +0000

    Add files via upload

---
+++ teste Array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class teste_Arrays
+{
+     public static void Main(string[] args)
+     {
+        //Array
+        int[] idadeAlunos = { 12, 14, 30, 17};
+        //percorrer com array com for//
+        Console.WriteLine("idade percorrida com for: ");
+        for (int contador = 0; contador < idadeAlunos.Length; contador++)
+        {
+            Console.WriteLine(idadeAlunos[contador]);
+        }
+        //percorrer com array com foreach//
+        Console.WriteLine("idade percorrida com foreach: ");
+        foreach (int idade in idadeAlunos) 
+        {
+            Console.WriteLine(idade);
+        }
+        
+     }
+}
