f0effd3ab3c68700d982688da1c7cc885fcdccb7
rafaelmarques0 <rafael20marques08@gmail.com>
2025-02-12 10:32:29 +0000

    Add files via upload

---
+++ exercicio 17.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Exercicio_17
+{
+    
+      public static void Main(string[] args)
+      {
+        //Arrays //
+        String[] ListaFilmes = {"Transformers", "Sherk 2","Tictanic" };
+        int[] DatasFilmes = { 2008, 2010, 2000 };
+
+        //faz uma list com o For e Arrays//
+        Console.WriteLine("listas de Filmes: ");
+        for (int contador = 0; contador < ListaFilmes.Length; contador++)
+        {
+                Console.WriteLine(ListaFilmes[contador]+" - " + DatasFilmes[contador]); 
+             
+        }
+
+    }
+}
---
+++ exercicio 18.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Exercicio_18
+{
+      public static void Main(string[] args)
+      {
+        //Array das Cidades//
+        String[] Cidades = { "Lisboa", "Porto", "Coimbra", "Faro", "Braga" };
+        
+        //Array da populaçao//
+        int[] populacao = { 545000, 237000, 143000, 118000, 193000 };
+        String[] Habitantes = { "Habitantes", "Habitantes", "Habitantes", "Habitantes", "Habitantes" };
+
+
+        // fazeer lista de cidades e a sua populacao//
+        Console.WriteLine("Cidades e as suas população");
+        for (int contador= 0; contador < Cidades.Length; contador++)
+        {
+            Console.WriteLine(Cidades[contador]+" - " + populacao[contador] +" "+ Habitantes[contador]);
+        }
+      }
+}
